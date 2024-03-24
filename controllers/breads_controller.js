c61afce10b713518e385d388bbc1132fd4e8e7eb
Samantha Cruz <cruz.s98319@gmail.com>
2024-03-24 11:18:11 -0700

    Edit and update routes complete

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -31,6 +31,14 @@ breads.get('/new', (req, res) => {
     res.render('new')
 })
 
+// EDIT
+breads.get('/:indexArray/edit', (req, res) => {
+    res.render('edit', {
+      bread: Bread[req.params.indexArray],
+      index: req.params.indexArray
+    })
+})
+
 // SHOW
 breads.get('/:arrayIndex', (req, res) => {
     if (Bread[req.params.arrayIndex]) {
@@ -43,11 +51,21 @@ breads.get('/:arrayIndex', (req, res) => {
     }
 })
 
+// UPDATE
+breads.put('/:arrayIndex', (req, res) => {
+    if(req.body.hasGluten === 'on'){
+      req.body.hasGluten = true
+    } else {
+      req.body.hasGluten = false
+    }
+    Bread[req.params.arrayIndex] = req.body
+    res.redirect(`/breads/${req.params.arrayIndex}`)
+  })  
 
 // DELETE
 breads.delete('/:indexArray', (req, res) => {
     Bread.splice(req.params.indexArray, 1)
     res.status(303).redirect('/breads')
   })
-  
+
 module.exports = breads

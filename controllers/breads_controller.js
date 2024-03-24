3a1926e710c5d88f2c33e45306ad60af5abf372c
Samantha Cruz <cruz.s98319@gmail.com>
2024-03-24 12:10:06 -0700

    Implemented helper methods

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,26 +4,36 @@ const Bread = require('../models/breads.js')
 
 // INDEX
 breads.get('/', (req, res) => {
-    res.render('Index',
-        {
-            breads: Bread
-        }
-    )
+    Bread.find()
+        .then(foundBreads => {
+            res.render('index', {
+                breads: foundBreads,
+                title: 'Index Page'
+            })
+        })
+    // res.render('index',
+    //   {
+    //     breads: Bread,
+    //     title: 'Index Page'
+    //   }
+    // )
 })
 
 // CREATE
 breads.post('/', (req, res) => {
-    if (!req.body.image) {
-      req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+    if(!req.body.image) {
+        req.body.image = undefined 
     }
     if(req.body.hasGluten === 'on') {
       req.body.hasGluten = true
     } else {
       req.body.hasGluten = false
     }
-    Bread.push(req.body)
+    Bread.create(req.body)
     res.redirect('/breads')
   })
+  
+
   
 
 // NEW
@@ -40,15 +50,26 @@ breads.get('/:indexArray/edit', (req, res) => {
 })
 
 // SHOW
-breads.get('/:arrayIndex', (req, res) => {
-    if (Bread[req.params.arrayIndex]) {
-        res.render('Show', {
-        bread:Bread[req.params.arrayIndex],
-        index: req.params.arrayIndex,
+// breads.get('/:arrayIndex', (req, res) => {
+//     if (Bread[req.params.arrayIndex]) {
+//         res.render('Show', {
+//         bread:Bread[req.params.arrayIndex],
+//         index: req.params.arrayIndex,
+//         })
+//     } else {
+//         res.render('404')
+//     }
+// })
+breads.get('/:id', (req, res) => {
+    Bread.findById(req.params.id)
+        .then(foundBread => {
+            res.render('show', {
+                bread: foundBread
+            })
         })
-    } else {
-        res.render('404')
-    }
+        .catch(err => {
+            res.send('404')
+        })
 })
 
 // UPDATE
@@ -63,9 +84,15 @@ breads.put('/:arrayIndex', (req, res) => {
   })  
 
 // DELETE
-breads.delete('/:indexArray', (req, res) => {
-    Bread.splice(req.params.indexArray, 1)
-    res.status(303).redirect('/breads')
+// breads.delete('/:indexArray', (req, res) => {
+//     Bread.splice(req.params.indexArray, 1)
+//     res.status(303).redirect('/breads')
+//   })
+breads.delete('/:id', (req, res) => {
+    Bread.findByIdAndDelete(req.params.id) 
+      .then(deletedBread => { 
+        res.status(303).redirect('/breads')
+      })
   })
-
+  
 module.exports = breads
--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 // DEPENDENCIES
 const express = require('express')
+const mongoose = require('mongoose')
 
 // CONFIGURATION
 require('dotenv').config()
@@ -33,3 +34,5 @@ app.get('*', (req, res) => {
 app.listen(PORT, () => {
     console.log('listening on port', PORT);
 })
+
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}).then(() => console.log('connected to mongo: ', process.env.MONGO_URI))
